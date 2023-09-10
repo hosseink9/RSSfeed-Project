afb20d6cf7a7fb17b693488656ad951a2f430e1d
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-10 19:35:36 +0330

    add str for both model

--- src/author/models.py
+++ src/author/models.py
@@ -4,6 +4,12 @@ from django.db import models
 class PodcastAuthor(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+
 
 class EpisodeAuthor(models.Model):
     name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
