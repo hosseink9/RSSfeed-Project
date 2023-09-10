4f79f03e179456f5e4356b0ceabf5dd2fe00593e
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-10 20:34:13 +0330

    Create Episode model

--- src/episode/models.py
+++ src/episode/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from podcast.models import Podcast
+from main.models import BaseModel
+from author.models import EpisodeAuthor
+
+class Episode(BaseModel):
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True,blank=True)
+    pubDate = models.DateTimeField()
+    link = models.URLField(null=True,blank=True)
+    guid = models.CharField(max_length=50)
+    itunes_summery = models.TextField()
+    podcast = models.ForeignKey(Podcast,on_delete=models.CASCADE)
+    episode_author = models.ForeignKey(EpisodeAuthor,on_delete=models.SET_NULL)
+
+
+    def __str__(self):
+        return self.title
