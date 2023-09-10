422607d08f15bb9b20b07e46cbac0d92cec34ca4
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-10 20:19:58 +0330

    create podcast model and IMage model

--- src/podcast/models.py
+++ src/podcast/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from main.models import BaseModel
+from author.models import PodcastAuthor
 
 
 class Category(BaseModel):
@@ -16,3 +17,22 @@ class Owner(BaseModel):
 
     def __str__(self):
         return self.name
+
+
+class Image(BaseModel):
+    link = models.URLField()
+    title = models.CharField(max_length=100)
+    url = models.URLField()
+
+
+class Podcast(BaseModel):
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True,blank=True)
+    pubDate = models.DateTimeField()
+    link = models.URLField(null=True,blank=True)
+    language = models.CharField(max_length=50)
+    itunes_subtitle = models.TextField(null=True,blank=True)
+    itunes_type = models.CharField(max_length=50)
+    category = models.ManyToManyField(Category)
+    podcast_author = models.ForeignKey(PodcastAuthor,on_delete=models.SET_NULL)
+    image = models.ForeignKey(Image,on_delete=models.SET_NULL)
