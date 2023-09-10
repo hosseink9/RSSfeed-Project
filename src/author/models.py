e8ca2d2a27fffa293eedb11d29214ad2aad6c7a3
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-10 19:59:22 +0330

    add basemodel in author models and parent all modl

--- src/author/models.py
+++ src/author/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 
 
-class PodcastAuthor(models.Model):
+from main.models import BaseModel
+
+class PodcastAuthor(BaseModel):
     name = models.CharField(max_length=30)
 
     def __str__(self):
         return self.name
 
 
-class EpisodeAuthor(models.Model):
+class EpisodeAuthor(BaseModel):
     name = models.CharField(max_length=30)
 
     def __str__(self):
