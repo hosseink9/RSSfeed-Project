c404b80569aeb6857b343055a432d8fb6895d0b5
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-10 19:34:49 +0330

    Create EpisodeAuthor

--- src/author/models.py
+++ src/author/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class PodcastAuthor(models.Model):
     name = models.CharField(max_length=30)
 
+
+class EpisodeAuthor(models.Model):
+    name = models.CharField(max_length=30)
