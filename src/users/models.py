d1e67ccf51aab91f41050a053e05ee8b7261d59c
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-10 19:39:18 +0330

    add UserManager

--- src/users/models.py
+++ src/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 # Create your models here.
 
 
@@ -10,3 +11,26 @@ def phone_validator(phone:str):
     if not (matched := re.fullmatch(PHONE_REGEX_PATTERN, phone.strip())):
         raise ValidationError("Invalid phone number")
     return matched
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, phone, password, **other_fields):
+        if phone is None:
+            raise ValueError("Phone not given")
+
+        user = self.model(phone=phone, **other_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone, password=None, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(phone, password, **other_fields)
