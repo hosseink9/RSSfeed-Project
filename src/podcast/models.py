c2aac1a8a89d400f624581c089488bdd4ed63248
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-11 17:15:38 +0330

    edit some fields in Podcast model

--- src/podcast/models.py
+++ src/podcast/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from main.models import BaseModel
-from author.models import Author
+from author.models import PodcastAuthor
 
 
 class Category(BaseModel):
@@ -41,9 +41,10 @@ class Podcast(BaseModel):
     language = models.CharField(max_length=50)
     itunes_type = models.CharField(max_length=50)
     copy_right = models.CharField(max_length=100)
-
+    explicit = models.CharField(max_length=50)
     #We use description for itunes summary and handel this in view
-    description = models.TextField(null=True,blank=True)
+    description = models.TextField()
+
     pubDate = models.DateTimeField(null=True,blank=True)
     last_build_date = models.DateTimeField(null=True,blank=True)
     link = models.URLField(null=True,blank=True)
@@ -51,7 +52,8 @@ class Podcast(BaseModel):
     itunes_keywords = models.TextField(null=True, blank=True)
 
     category = models.ManyToManyField(Category)
-    author = models.ForeignKey(Author,on_delete=models.SET_NULL)
+    generator = models.ForeignKey(Generator,on_delete=models.CASCADE)
+    podcast_author = models.ForeignKey(PodcastAuthor,on_delete=models.SET_NULL)
     image = models.OneToOneField(Image,on_delete=models.SET_NULL)
 
     def __str__(self):
