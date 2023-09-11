af84cf7548b6be433b0028acd0dd137f286ae5fe
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-11 15:13:01 +0330

    Fix: add null and blank for episode author model

--- src/author/models.py
+++ src/author/models.py
@@ -11,7 +11,7 @@ class PodcastAuthor(BaseModel):
 
 
 class EpisodeAuthor(BaseModel):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30,null=True,blank=True)
 
     def __str__(self):
         return self.name
