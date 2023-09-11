0bfce385ef167b6743c2d3c7850a4bbf37e0ddbe
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-11 15:53:09 +0330

    fix author field and some another fields

--- src/episode/models.py
+++ src/episode/models.py
@@ -2,19 +2,23 @@ from django.db import models
 
 from podcast.models import Podcast
 from main.models import BaseModel
-from author.models import EpisodeAuthor,PodcastAuthor
+from author.models import Author
 
 class Episode(BaseModel):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100) #We use title for itunes title and handel it in view
     guid = models.CharField(max_length=50)
+    itunes_duration = models.CharField(max_length=50)
+    itunes_episodeType = models.CharField(max_length=50)
+    itunes_explicit = models.CharField(max_length=50)
 
-    description = models.TextField(null=True,blank=True) #We use description for itunes summary and handel this in view
+    #We use description for itunes summary and content_encoded and handel this in view
+    description = models.TextField(null=True,blank=True)
     link = models.URLField(null=True,blank=True)
     pubDate = models.DateTimeField(null=True,blank=True)
     itunes_keywords = models.TextField(null=True, blank=True)
 
     podcast = models.ForeignKey(Podcast,on_delete=models.CASCADE)
-    episode_author = models.ForeignKey(EpisodeAuthor,on_delete=models.SET_NULL)
+    author = models.ForeignKey(Author,on_delete=models.CASCADE)
 
 
     def __str__(self):
