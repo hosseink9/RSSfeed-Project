29dbac54d835074d803501d5ef6f6298594bb74f
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-11 17:12:38 +0330

    add EpisodeAuthor and change author name

--- src/author/models.py
+++ src/author/models.py
@@ -2,8 +2,16 @@ from django.db import models
 
 from main.models import BaseModel
 
-class Author(BaseModel):
+class PodcastAuthor(BaseModel):
     name = models.CharField(max_length=30)
+
+
+    def __str__(self):
+        return self.name
+
+
+class EpisodeAuthor(BaseModel):
+    name = models.CharField(max_length=30, null=True, blank=True)
 
 
     def __str__(self):
