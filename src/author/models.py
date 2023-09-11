e9ab797de87e939d721b7d50091f4e048a7341f4
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-11 15:52:17 +0330

    fix models in author models

--- src/author/models.py
+++ src/author/models.py
@@ -1,17 +1,10 @@
 from django.db import models
 
-
 from main.models import BaseModel
 
-class PodcastAuthor(BaseModel):
+class Author(BaseModel):
     name = models.CharField(max_length=30)
 
-    def __str__(self):
-        return self.name
-
-
-class EpisodeAuthor(BaseModel):
-    name = models.CharField(max_length=30,null=True,blank=True)
 
     def __str__(self):
         return self.name
