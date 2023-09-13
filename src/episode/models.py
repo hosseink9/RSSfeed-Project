dbfee75cf282fc9638e39f46a858ca398b9cb284
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-13 11:49:09 +0330

    fix some fields in Episode

--- src/episode/models.py
+++ src/episode/models.py
@@ -5,25 +5,19 @@ from main.models import BaseModel
 from author.models import EpisodeAuthor
 
 class Episode(BaseModel):
-    title = models.CharField(max_length=100) #We use title for itunes title and handel it in view
+    title = models.CharField(max_length=100) #We use title for itunes_title too.
     guid = models.CharField(max_length=50)
     itunes_duration = models.CharField(max_length=50)
     itunes_episode_type = models.CharField(max_length=50)
     itunes_explicit = models.CharField(max_length=50)
-    #We use description for itunes summary and content_encoded and handel this in view
-    description = models.TextField()
+    description = models.TextField() #We use description for itunes summary and content_encoded too.
     enclosure = models.CharField(null=True, blank=True)
-
     link = models.URLField(null=True,blank=True)
     pub_date = models.DateTimeField(null=True,blank=True)
     itunes_keywords = models.TextField(null=True, blank=True)
-
-    #We use itunes_player for fireside_playerEmbedCode and handel this in view
-    itunes_player = models.CharField(null=True, blank=True)
-
+    itunes_player = models.CharField(null=True, blank=True) #We use itunes_player for fireside_playerEmbedCode too.
     podcast = models.ForeignKey(Podcast,on_delete=models.CASCADE)
     episode_author = models.ForeignKey(EpisodeAuthor,on_delete=models.CASCADE)
 
-
     def __str__(self):
-        return self.title
+        return f"{self.id}: {self.title}"
