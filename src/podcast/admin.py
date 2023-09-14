141cff200fce1eec9833e680baff9ea153e94025
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-14 16:12:49 +0330

    register all models in podcast app

--- src/podcast/admin.py
+++ src/podcast/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Podcast, Category, Generator, Image, Owner
 
 # Register your models here.
+admin.site.register(Podcast)
+admin.site.register(Category)
+admin.site.register(Generator)
+admin.site.register(Image)
+admin.site.register(Owner)
