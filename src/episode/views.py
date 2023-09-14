1d274e2f31638aec938a28694ecc02374303d243
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-14 16:11:19 +0330

    create view for episode app

--- src/episode/views.py
+++ src/episode/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Episode
+from .serializers import EpisodeSerializer
+
+class EpisodeListView(APIView):
+    def get(self, request):
+        queryset = Episode.objects.all()
+        serializer_data = EpisodeSerializer(queryset, many=True)
+        return Response(serializer_data.data, status=status.HTTP_200_OK)
+
+
