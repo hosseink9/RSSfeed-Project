00aba3b698148565c4f55d524e68e46c70b98b5a
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-14 16:14:41 +0330

    Create podcast list view in podcast views

--- src/podcast/views.py
+++ src/podcast/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from .models import Podcast
+from .serializer import PodcastSerializer
+from .utils import Parser
 
 # Create your views here.
+class PodcastListView(APIView):
+    def get(self, request):
+        query = Podcast.objects.all()
+        ser_data = PodcastSerializer(instance=query, many=True)
+        return Response(ser_data.data, status=status.HTTP_200_OK)
+
