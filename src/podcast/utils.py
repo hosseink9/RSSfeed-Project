bffeabb7b0479e3d1eb80bb135d2b81b8953b085
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-14 16:22:59 +0330

    Create a Episode model in utils file

--- src/podcast/utils.py
+++ src/podcast/utils.py
@@ -7,3 +7,13 @@ from author.models import EpisodeAuthor, PodcastAuthor
 class PodcastModel:
     def __init__(self, xml=None):
         self.xml = xml
+
+class EpisodeModel:
+    def __init__(self,podcast, title=None,author=None,guid=None,description=None,pubDate=None,link=None,itunes_title=None) -> None:
+        self.title=title
+        self.author=author
+        self.guid=guid
+        self.description=description
+        self.pubDate=pubDate
+        self.link=link
+        self.itunes_title=itunes_title
