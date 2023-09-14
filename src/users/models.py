29ddcd510642b715e109fb9f80444fad429e9905
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-14 16:20:39 +0330

    Completed User model

--- src/users/models.py
+++ src/users/models.py
@@ -1,9 +1,10 @@
+import re
+
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.core.exceptions import ValidationError
-from django.contrib.auth.models import AbstractUser, BaseUserManager
-# Create your models here.
 
-
+from main.models import BaseModel
 
 PHONE_REGEX_PATTERN = r"(((\+|00)(98))|0)?(?P<operator>9\d{2})-?(?P<middle3>\d{3})-?(?P<last4>\d{4})"
 
@@ -34,3 +35,39 @@ class UserManager(BaseUserManager):
             raise ValueError('Superuser must have is_superuser=True.')
 
         return self.create_user(phone, password, **other_fields)
+
+
+class PhoneNumberField(models.CharField):
+    def get_prep_value(self, value):
+        if value is None:
+            return value
+
+        try:
+            regex = phone_validator(value)
+        except ValidationError:
+            raise
+
+        phone_parts = regex.groupdict()
+        phone = phone_parts["operator"]+phone_parts["middle3"]+phone_parts["last4"]
+        return phone
+
+
+
+class User(AbstractBaseUser,PermissionsMixin, BaseModel):
+    phone = PhoneNumberField(validators=[phone_validator], unique=True, max_length=20)
+
+    username = models.CharField(max_length=50,unique=True)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"{self.phone}"
