491bad3afcd68adefd8a55bf345a8916163d2d52
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-14 16:08:44 +0330

    add apps and some settings for cors

--- src/config/settings.py
+++ src/config/settings.py
@@ -40,12 +40,16 @@ INSTALLED_APPS = [
     'rest_framework',
     'author',
     'podcast',
+    'main',
+    'episode',
     'users',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,3 +129,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "users.User"
+
+AUTHENTICATION_BACKENDS = [
+    'users.auth.UserAuthBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALL_CREDENTIALS = True
