32fae94793bda7b32f4fd23e9c3cba3e32ee7e3d
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-14 16:24:54 +0330

    Create a func for get podcast data in Parser class

--- src/podcast/utils.py
+++ src/podcast/utils.py
@@ -35,3 +35,22 @@ class Parser:
         with open(self.rss_path, "rt", encoding="utf-8") as file:
             rss_file = file.read()
         return rss_file
+
+    def get_podcast_data(self):
+        rss_excluded_episodes = re.sub("<item>(?:.|\n)*?<\/item>","", self.rss_file)
+        match_list = self.tag_pattern.finditer(rss_excluded_episodes)
+        podcast_tags_name = list(set([item.group(1).replace('/', "") for item in match_list])) #/title -> title
+        podcast = PodcastModel()
+
+        for tag in podcast_tags_name:
+            pattern = f"(?:<{tag}.*?>)(.*?)(?:<\/{tag}>)"
+            close_pattern = f"(?:<itunes:category+)(.*?)(?:/>)"
+            category_set = re.findall(close_pattern, str(rss_excluded_episodes))
+            key_value_pattern = '([^ ]*?)=\"(.*?)\"'
+            category_value = re.findall(key_value_pattern, str(category_set))
+
+            values_item = re.findall(pattern,str(rss_excluded_episodes))
+            values_item = list(set(values_item))
+            podcast.__setattr__(tag.replace(":","_"),values_item[0]) if len(values_item) == 1 else podcast.__setattr__(tag.replace(":","_"),values_item)
+        podcast.itunes_category = category_value[0][1]
+        return podcast
