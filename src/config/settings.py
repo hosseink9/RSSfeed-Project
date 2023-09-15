1fd6f950066078646e724e8dbc475d6a538aa673
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-15 09:25:28 +0330

    Add postgres db setting

--- src/config/settings.py
+++ src/config/settings.py
@@ -89,6 +89,18 @@ DATABASES = {
 }
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'podcast',
+#         'USER': 'hosseink9',
+#         'PASSWORD': '384662',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
