6b660316c363c74956e41c1e2f351ff535c9a68b
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-23 18:47:08 +0330

    Create Comment Serializer

--- src/feedback/serializeres.py
+++ src/feedback/serializeres.py
@@ -6,3 +6,9 @@ class LikeSerializer(ModelSerializer):
     class META:
         model = Like
         fields = ['user','content_type']
+
+
+class CommentSerializer(ModelSerializer):
+    class META:
+        model = Comment
+        fields = ['user','text','content_type',]
