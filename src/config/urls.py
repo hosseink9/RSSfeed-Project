3b5ba0c82ea2a4d4156815c069398dcaa2e51dc6
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-23 18:44:41 +0330

    Add feedback app url in config urls

--- src/config/urls.py
+++ src/config/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('podcast/', include('podcast.urls')),
     path('episode/', include('episode.urls')),
     path('users/', include('users.urls')),
+    path('feedback/', include('feedback.urls')),
+
     ]
