9f33c3c186f5a2b8bae2ff140e589147c50463f8
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-25 15:55:22 +0330

    Create Playlist Model

--- src/feedback/models.py
+++ src/feedback/models.py
@@ -5,22 +5,36 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from main.models import BaseModel
 from users.models import User
 from podcast.models import Podcast
+from episode.models import Episode
 
 class Like(BaseModel):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    account = models.ForeignKey(User,on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey("content_type", "object_id")
 
     def __str__(self):
-        return self.user
+        return self.account
 
 class Comment(BaseModel):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey("content_type", "object_id")
 
     def __str__(self):
-        return self.user
+        return self.account.phone
+
+
+class Playlist(BaseModel):
+    title = models.CharField(max_length=40)
+    description = models.TextField(blank=False,null=True)
+    account = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    podcasts = models.ManyToManyField(Podcast)
+    episodes = models.ManyToManyField(Episode)
+
+
+    def __str__(self) -> str:
+        return self.title
