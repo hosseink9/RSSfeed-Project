7df8dfab842229c464aa77651991ba3e5132607d
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-25 15:57:43 +0330

    Add some urls for like and comment and playlist

--- src/podcast/urls.py
+++ src/podcast/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import PodcastListView, AddPodcastView
+from .views import PodcastListView, AddPodcastView, LikeView, CommentView, PlaylistView
 
 urlpatterns = [
     path("podcasts/", PodcastListView.as_view(), name="podcasts"),
-    path("add_podcast/", AddPodcastView.as_view(),name='add_podcast')
+    path("add_podcast/", AddPodcastView.as_view(),name='add_podcast'),
+    path("like/", LikeView.as_view(),name='like'),
+    path("comment/", CommentView.as_view(),name='comment'),
+    path("playlist/", PlaylistView.as_view(),name='playlist'),
+
 ]
