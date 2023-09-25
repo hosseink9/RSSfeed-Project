78adde5df1ab98b4303a9de229a2971d137d8180
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-25 15:59:58 +0330

    Add serializer Login

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -17,3 +17,29 @@ class UserSerializer(serializers.ModelSerializer):
             raise ("You don't add password" )
         instance.save()
         return instance
+
+
+class SerializerLogin(serializers.Serializer):
+
+    phone = serializers.CharField(required=True, allow_null=False)
+
+
+    def validate(self, data):
+        phone = data.get('phone')
+
+        if not User.objects.filter(phone=phone).exists():
+            raise serializers.ValidationError
+
+
+
+        return data
+
+    @staticmethod
+    def create_otp(request, phone):
+        request.session["otp"] = random.randint(1000, 9999)
+        request.session["otp_expire"] = (timezone.now() + timedelta(minutes=10)).strftime("%d/%m/%Y, %H:%M:%S")
+        request.session['phone']=phone
+        print(f"generated:{request.session['otp']}  until:{request.session['otp_expire']}")
+
+
+
