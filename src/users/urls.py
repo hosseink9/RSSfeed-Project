e3ea321cbd99243d47bb6befc3f4b9a9ac085829
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-25 16:00:36 +0330

    Add some urls for log in and otp

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import RegisterView,LoginView
+from .views import RegisterView, SendOTPView,VerifyOTP,LoginRequiredView, LogoutAPIView
 
 app_name = 'users'
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login')
+    path('sendotp/',SendOTPView.as_view()),
+    path('verify/', VerifyOTP.as_view()),
+    path('requiredlogin/', LoginRequiredView.as_view()),
+    path('logout/', LogoutAPIView.as_view()),
+
 ]
