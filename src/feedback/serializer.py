db738f58ce6a861e5857a9eb13f20495bac0c5ea
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-26 18:56:33 +0330

    Delete and fix some packages

--- src/feedback/serializer.py
+++ src/feedback/serializer.py
@@ -1,8 +1,6 @@
 from rest_framework import serializers
-from .models import Like, Comment, Playlist
-from podcast.models import Podcast
-from episode.models import Episode
-from users.models import User
+from .models import Playlist
+
 
 
 class LikeSerializer(serializers.Serializer):
