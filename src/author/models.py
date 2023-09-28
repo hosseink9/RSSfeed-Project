c6dccd6a992ab2db38f2db47152a8677f8974f77
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-28 16:22:09 +0330

    Fix name field for EpisodeAuthor models

--- src/author/models.py
+++ src/author/models.py
@@ -11,8 +11,8 @@ class PodcastAuthor(BaseModel):
 
 
 class EpisodeAuthor(models.Model):
-    name = models.CharField(max_length=30, null=True, blank=True)
+    name = models.CharField(max_length=30, unique=True)
 
 
     def __str__(self):
-        return self.name
+        return f'{self.name}'
