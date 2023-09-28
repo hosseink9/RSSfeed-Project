e9cf2d88b0a71f16c9f6270d0d1fc15b54cd0865
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-28 16:28:19 +0330

    fix some fields for Episode model

--- src/episode/models.py
+++ src/episode/models.py
@@ -9,7 +9,7 @@ class Episode(models.Model):
     guid = models.CharField(max_length=50)
     itunes_duration = models.CharField(max_length=50)
     itunes_episode_type = models.CharField(max_length=50)
-    itunes_explicit = models.CharField(max_length=50)
+    itunes_explicit = models.CharField(max_length=50,null=True,blank=True)
     description = models.TextField() #We use description for itunes summary and content_encoded too.
     enclosure = models.CharField(null=True, blank=True,max_length=100)
     link = models.URLField(null=True,blank=True)
@@ -17,7 +17,7 @@ class Episode(models.Model):
     itunes_keywords = models.TextField(null=True, blank=True)
     itunes_player = models.CharField(null=True, blank=True,max_length=100) #We use itunes_player for fireside_playerEmbedCode too.
     episode_podcast = models.ForeignKey(Podcast,on_delete=models.CASCADE, related_name="episode")
-    episode_author = models.ForeignKey(EpisodeAuthor,on_delete=models.CASCADE)
+    episode_author = models.ForeignKey(EpisodeAuthor,on_delete=models.CASCADE,null=True)
 
     def __str__(self):
         return f"{self.id}: {self.title}"
