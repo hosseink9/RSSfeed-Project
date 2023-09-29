8946546f0c9f01f5c86af78220f1f7be2c02b268
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-29 10:03:02 +0330

    Add some packages and files and logger

--- src/podcast/tasks.py
+++ src/podcast/tasks.py
@@ -1,1 +1,12 @@
-from celery import shared_task
+from celery import shared_task, Task
+from django.db.models import Max
+import datetime as dt
+from time import sleep
+import logging
+
+from .models import Podcast
+from episode.models import Episode
+from author.models import PodcastAuthor, EpisodeAuthor
+
+logger = logging.getLogger('django-celery')
+
