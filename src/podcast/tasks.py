6bb0995c8c5c6e39768d938e8bc6874f46870d65
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-29 10:03:48 +0330

    Create a Base class fo celery setup retry task

--- src/podcast/tasks.py
+++ src/podcast/tasks.py
@@ -10,3 +10,12 @@ from author.models import PodcastAuthor, EpisodeAuthor
 
 logger = logging.getLogger('django-celery')
 
+
+class RetryTask(Task):
+    autoretry_for = (Exception, KeyError)
+    retry_kwargs = {'max_retries': 5}
+    retry_backoff = 2
+
+
+
+
