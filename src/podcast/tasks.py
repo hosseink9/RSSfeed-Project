d0f9dbad9514ee14c4d7a5afed037e17a1fae02d
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-29 10:04:21 +0330

    Create a func for give authors of ep or podcast

--- src/podcast/tasks.py
+++ src/podcast/tasks.py
@@ -18,4 +18,19 @@ class RetryTask(Task):
 
 
 
+@shared_task(bind=True, base=RetryTask)
+def author(self,episode_list):
+    author_list = list()
+    dict_list = dict()
+
+    for episode in episode_list:
+        author = dict_list.get(episode.get("itunes_author")) or EpisodeAuthor.objects.get_or_create(name=episode.get("itunes_author"))[0] if episode.get('itunes_author') else None
+        dict_list[episode.get("itunes_author")] = author
+        author_list.append(author)
+
+    author_list = list(map(lambda author:author.id if author else None,author_list))
+    return author_list
+
+
+
 
