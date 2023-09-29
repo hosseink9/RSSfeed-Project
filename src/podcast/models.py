38cbdc6fbcd05af9171a162c721ec5055e0cab95
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-29 10:01:46 +0330

    Add str for Image model

--- src/podcast/models.py
+++ src/podcast/models.py
@@ -25,6 +25,9 @@ class Image(BaseModel):
     title = models.CharField(max_length=100,null=True,blank=True)
     link = models.URLField(null=True,blank=True)
 
+    def __str__(self):
+        return f'{self.url}'
+
 
 class Generator(BaseModel):
     name = models.CharField(max_length=100)
