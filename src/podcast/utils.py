7f0176c6c69e3f3fbe232db9fb67f0fba86191f4
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-29 10:12:41 +0330

    Fixed some codes and lines in utils file for parsing rss

--- src/podcast/utils.py
+++ src/podcast/utils.py
@@ -1,36 +1,25 @@
 import re
 import datetime as dt
 from .models import Podcast,Category, Generator, Image, Owner
-from episode.models import Episode
-from author.models import EpisodeAuthor, PodcastAuthor
-from django.db.models import Max
+from author.models import PodcastAuthor
+from .tasks import update_task, save_episode, author
 
-class PodcastModel:
-    def __init__(self, xml=None):
-        self.xml = xml
-
-class EpisodeModel:
-    def __init__(self,podcast, title=None,author=None,guid=None,description=None,pubDate=None,link=None,itunes_title=None) -> None:
-        self.title=title
-        self.author=author
-        self.guid=guid
-        self.description=description
-        self.pubDate=pubDate
-        self.link=link
-        self.itunes_title=itunes_title
 
 class Parser:
     item_tag_pattern = re.compile("<item>((\n|.)*?)</item>")
-    tag_pattern = re.compile("<([^\<\> ]*)(?:\s*(\S*)=\"([^\"]*?)\"[^\>]*)*>")
+    tag_pattern = re.compile("<([^\<\>\[\]\- ]*)(?:\s*(\S*)=\"([^\"]*?)\"[^\>\]\[]*)*>")
     key_value_pair_pattern = re.compile('([^ ]*?)=\"(.*?)\"')
 
     def __init__(self, rss_path=None, rss_file=None, save=False):
         self.rss_path = rss_path
         self.rss_file = rss_file or self._read_rss_file()
-        self.podcast_obj = None
+        podcast_data = self.get_podcast_data()
+        podcast_object = Podcast.objects.filter(title = podcast_data.get("title"),link= podcast_data.get("link"))
+        self.podcast_object = podcast_object.get() if podcast_object else None
         self.episodes_obj = list()
-        self.save_podcast_in_db() if save is True else None
-        self.save_episode_in_db() if save is True else None
+        if save:
+            self.save_podcast_in_db() if not self.check_exist()[0] else None
+            self.save_episode_in_db() if not self.check_exist()[1] else None
 
     def _read_rss_file(self):
         with open(self.rss_path, "rt", encoding="utf-8") as file:
@@ -41,7 +30,7 @@ class Parser:
         rss_excluded_episodes = re.sub("<item>(?:.|\n)*?<\/item>","", self.rss_file)
         match_list = self.tag_pattern.finditer(rss_excluded_episodes)
         podcast_tags_name = list(set([item.group(1).replace('/', "") for item in match_list])) #/title -> title
-        podcast = PodcastModel()
+        podcast = {}
 
         for tag in podcast_tags_name:
             pattern = f"(?:<{tag}.*?>)(.*?)(?:<\/{tag}>)"
@@ -51,145 +40,100 @@ class Parser:
             close_pattern_image = f"(?:<itunes:image+)(.*?)(?:/>)"
             image_set = re.findall(close_pattern_image, str(rss_excluded_episodes))
 
+            close_pattern_atom_link = f"(?:<atom:link+)(.*?)(?:/>)"
+            atom_link_set = re.findall(close_pattern_atom_link, str(rss_excluded_episodes))
+
             key_value_pattern = '([^ ]*?)=\"(.*?)\"'
             category_value = re.findall(key_value_pattern, str(category_set))
 
             image_value = re.findall(key_value_pattern, str(image_set))
 
+            atom_link_value = re.findall(key_value_pattern, str(atom_link_set))
+
             values_item = re.findall(pattern,str(rss_excluded_episodes))
             values_item = list(set(values_item))
-            podcast.__setattr__(tag.replace(":","_"),values_item[0]) if len(values_item) == 1 else podcast.__setattr__(tag.replace(":","_"),values_item)
-        podcast.itunes_category = category_value[0][1]
-        podcast.itunes_image = image_value[0][1]
+            podcast[tag.replace(":","_")]="".join(values_item)
+        podcast["itunes_category"] = category_value[0][1]
+        podcast["itunes_image"] = image_value[0][1]
+        podcast["atom_link"] = atom_link_value[0][1] if len(atom_link_value) >= 1 else None
         return podcast
 
     def check_exist(self):
-        py_model = self.get_podcast_data()
-        episode_model = self.get_episode_data()
-        old_author = PodcastAuthor.objects.filter(name=py_model.itunes_author)
-        if old_author:
-            old_podcast = Podcast.objects.filter(title=py_model.title, link=py_model.link, podcast_author=old_author.first())
-            if old_podcast:
-                old_episode = Episode.objects.filter(guid = episode_model[0].guid)
-                if old_episode:
-                    return True
-        return False
+        podcast_dict = self.get_podcast_data()
+        old_podcast = Podcast.objects.filter(title=podcast_dict.get("title"), link=podcast_dict.get("link"))
+        if old_podcast:
+            if old_podcast.get().episode.all():
+                return True,True
+            return True,False
+        return False,False
 
     def get_episode_data(self):
-        podcast_obj = self.get_podcast_data()
+        podcast_dict = self.get_podcast_data()
         item_list = self.item_tag_pattern.findall(self.rss_file)
         episode_list = []
 
         for item in item_list:
-            episode1 = EpisodeModel(podcast=podcast_obj)
+            episode1 ={"podcast":podcast_dict}
             match_list = self.tag_pattern.finditer(item[0])
             episode_tags_name = list(set([item.group(1).replace('/', "") for item in match_list]))
 
-            for i in episode_tags_name:
-                pattern = f'(?:<{i}.*?>)(.*?)(?:<\/{i}>)'
+            for tag in episode_tags_name:
+                pattern = f'(?:<{tag}.*?>)(.*?)(?:<\/{tag}>)'
                 values_item = re.findall(pattern,str(item))
-                episode1.__setattr__(i.replace(":","_"),values_item[0]) if len(values_item) == 1 else episode1.__setattr__(i.replace(":","_"),values_item)
+                episode1[tag.replace(":","_")] = "".join(values_item)
             episode_list.append(episode1)
 
         return episode_list
 
     def save_podcast_in_db(self):
         #--save podcast--#
-        assert self.check_exist() == False, "Podcast already exist."
+        assert self.check_exist()[0] == False, "Podcast already exist."
         pod = self.get_podcast_data()
-        author = PodcastAuthor.objects.create(name=pod.itunes_author)
-        category = Category.objects.create(name=pod.itunes_category)
-        generator = Generator.objects.create(name=pod.generator)
-        image = Image.objects.create(url= pod.url if hasattr(pod, "url") else None )
-        owner = Owner.objects.create(name=pod.itunes_name, email=pod.itunes_email)
+        author = PodcastAuthor.objects.create(name=pod.get("itunes_author"))
+        category = Category.objects.create(name=pod.get("itunes_category"))
+        generator = Generator.objects.create(name=pod.get("generator")) if pod.get("generator") else None
+        image = Image.objects.create(url= pod.get("url",pod.get('itunes_image'))) if pod.get("url",pod.get('itunes_image')) else None
+        owner = Owner.objects.create(name=pod.get("itunes_name"), email=pod.get("itunes_email"))
 
         podcast_object = Podcast.objects.create(
-            title = pod.title,
-            language = pod.language,
-            itunes_type = pod.itunes_type,
-            copy_right = pod.copyright,
-            explicit = pod.itunes_explicit,
-            description = pod.description,
-            pubDate = dt.datetime.strptime(pod.pubDate, "%a, %d %b %Y %H:%M:%S %z") if hasattr(pod,'pubDate') else None, #“Mon, 4 Sep 2023 07:00:00 +0000”
-            last_build_date = dt.datetime.strptime(pod.lastBuildDate, "%a, %d %b %Y %H:%M:%S %z") if hasattr(pod,'last_build_date') else None,
-            link = pod.link,
-            itunes_subtitle = pod.itunes_subtitle if hasattr(pod, "itunes_subtitle") else None ,
-            itunes_keywords = pod.itunes_keywords if hasattr(pod, "itunes_keywords") else None,
-            itunes_image = pod.itunes_image,
+            title = pod.get("title"),
+            url = pod.get("atom_link"),
+            language = pod.get("language"),
+            itunes_type = pod.get("itunes_type"),
+            copy_right = pod.get("copyright"),
+            explicit = pod.get("itunes_explicit"),
+            description = pod.get("description"),
+            pubDate = dt.datetime.strptime(pod.get("pubDate"), "%a, %d %b %Y %H:%M:%S %z") if pod.get("pubDate") else None, #“Mon, 4 Sep 2023 07:00:00 +0000”
+            last_build_date = dt.datetime.strptime(pod.get("lastBuildDate"), "%a, %d %b %Y %H:%M:%S %z") if pod.get("lastBuildDate") else None,
+            link = pod.get("link"),
+            itunes_subtitle = pod.get("itunes_subtitle",None) ,
+            itunes_keywords = pod.get("itunes_keywords", None) ,
+            itunes_image = pod.get("itunes_image"),
             # category = category,
             podcast_generator = generator,
             podcast_author = author,
             podcast_image = image
         )
+        print(self.get_podcast_data())
         podcast_object.category.add(category)
         podcast_object.save()
-        self.podcast_obj = podcast_object
+        self.podcast_object = podcast_object
         return podcast_object
 
     def save_episode_in_db(self):
         #--save episode--#
-        assert self.check_exist() == False, "Episodes already exist."
+        assert self.check_exist()[1] == False, "Episodes already exist."
         episodes = self.get_episode_data()
+        author_list = author.delay(episodes).get()
 
-        author_list = self.get_author_objects(episodes)
-        self.save_episode(episodes, author_list, self.podcast_obj)
-
-
-    def get_author_objects(self,episode_list):
-        author_list = list()
-        dict_list = dict()
-
-        for i in episode_list:
-            author = dict_list.get(i.author) or EpisodeAuthor(name=i.author) #content@audiochuck.com (audiochuck) -> (audiochuck)content@audiochuck.com
-            dict_list[i.author] = author
-            author_list.append(author)
-
-        uniq_list = dict_list.values()
-        EpisodeAuthor.objects.bulk_create(uniq_list)
-        return author_list
+        self.parse_episode(episodes, author_list, self.podcast_object)
 
 
     def update_exist_podcast(self):
-        podcast = self.get_podcast_data()
-        episodes = self.get_episode_data()
-        author = PodcastAuthor.objects.filter(name=podcast.itunes_author).first()
-        podcast_object = Podcast.objects.get(title = podcast.title, link = podcast.link, podcast_author = author)
-        episode_objects_list = Episode.objects.filter(episode_podcast = podcast_object).values_list("guid",flat=True)
-
-        podcast_last_update = podcast.pubDate if hasattr(podcast,"pubDate") else max(list(map(lambda item:dt.datetime.strptime(item.pubDate,"%a, %d %b %Y %H:%M:%S %z"), episodes)))
-        podcast_object_last_update = podcast_object.pubDate if podcast_object.pubDate else Episode.objects.aggregate(Max("pubDate")).get("pubDate__max")
+        update = update_task.delay(self.get_podcast_data(),self.get_episode_data())
+        return update
 
 
-        episode_update_list = []
-        if podcast_object_last_update != podcast_last_update:
-            for new_episode in episodes:
-                if new_episode.guid not in episode_objects_list:
-                    episode_update_list.append(new_episode)
-            author_list = self.get_author_objects(episode_update_list)
-
-            self.save_episode(episode_update_list, author_list, podcast_object)
-
-
-    def save_episode(self,episode_list,author_list,podcast_object):
-        res_list = list()
-
-        for index, episode in enumerate(episode_list):
-            episode_field = Episode(
-                title = episode.title,
-                guid = episode.guid,
-                itunes_duration = episode.itunes_duration,
-                itunes_episode_type = episode.itunes_episodeType,
-                itunes_explicit = episode.itunes_explicit,
-                description = episode.description,
-                enclosure = episode.enclosure,
-                link = episode.link,
-                pubDate = dt.datetime.strptime(episode.pubDate, "%a, %d %b %Y %H:%M:%S %z"),
-                itunes_keywords = None if not hasattr(episode, "itunes_keywords") else episode.itunes_keywords,
-                itunes_player = None if not hasattr(episode, "itunes_player") else episode.itunes_player,
-                episode_podcast = podcast_object,
-                episode_author = author_list[index]
-            )
-            res_list.append(episode_field)
-        Episode.objects.bulk_create(res_list)
-
-
+    def parse_episode(self,episode_list,author_list,podcast_object):
+        episode = save_episode.delay(episode_list,author_list,podcast_object.id).get()
+        return episode
