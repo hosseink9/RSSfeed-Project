16555267b24255853743c41a5442466deb4f3317
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-29 13:40:05 +0330

    Add Like to admin register

--- src/feedback/admin.py
+++ src/feedback/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Comment, Playlist
+from .models import Comment, Playlist,Like
 
+admin.site.register(Like)
 admin.site.register(Comment)
 admin.site.register(Playlist)
