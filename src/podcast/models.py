7a00461adb176eb09f012624412b41f5a9b37d9e
hosseink9 <hosseinkalantari.29@gmail.com>
2023-10-01 20:43:35 +0330

    Add unique for name

--- src/podcast/models.py
+++ src/podcast/models.py
@@ -5,7 +5,7 @@ from author.models import PodcastAuthor
 
 
 class Category(BaseModel):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
