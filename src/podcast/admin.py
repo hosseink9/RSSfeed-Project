734b009e14e9f7c3fc2ea6ff387c8fa9bdc21391
hosseink9 <hosseinkalantari.29@gmail.com>
2023-10-01 20:41:56 +0330

    Register PodcastUrl in admin

--- src/podcast/admin.py
+++ src/podcast/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Podcast, Category, Generator, Image, Owner
+from .models import Podcast, Category, Generator, Image, Owner, PodcastUrl
 
 # Register your models here.
 admin.site.register(Podcast)
@@ -7,3 +7,4 @@ admin.site.register(Category)
 admin.site.register(Generator)
 admin.site.register(Image)
 admin.site.register(Owner)
+admin.site.register(PodcastUrl)
