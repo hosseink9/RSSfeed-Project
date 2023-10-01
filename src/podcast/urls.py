263d28a5f036f415d1f0b0d93b214337b1580d87
hosseink9 <hosseinkalantari.29@gmail.com>
2023-10-02 00:26:06 +0330

    Add add podcast url

--- src/podcast/urls.py
+++ src/podcast/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PodcastListView, AddPodcastView, LikeView, CommentView, PlaylistView, UpdatePodcastView
+from .views import PodcastListView, AddPodcastView, LikeView, CommentView, PlaylistView, UpdatePodcastView, AddPodcastUrlView
 
 urlpatterns = [
     path("podcasts/", PodcastListView.as_view(), name="podcasts"),
+    path("add_podcast_url/", AddPodcastUrlView.as_view(), name="podcasts"),
     path("add_podcast/", AddPodcastView.as_view(),name='add_podcast'),
     path("like/", LikeView.as_view(),name='like'),
     path("comment/", CommentView.as_view(),name='comment'),
