379c8ec52dcc5af2d7c52a8ae6aa32140d5f67ab
hosseink9 <hosseinkalantari.29@gmail.com>
2023-10-01 20:41:09 +0330

    Add log for update podcast

--- src/episode/tasks.py
+++ src/episode/tasks.py
@@ -1,7 +1,7 @@
 from celery import shared_task
 import requests
 from podcast.utils import Parser
-from .models import Episode
+from podcast.tasks import RetryTask
 from podcast.models import Podcast
 import logging
 
@@ -10,15 +10,22 @@ logger = logging.getLogger('django-celery')
 
 @shared_task
 def update_all_podcast():
-    url_list = Podcast.objects.all().values_list('url')
+    url_list = Podcast.objects.all().values_list('podcast_url')
     for url in url_list:
         update_podcast.delay(url)
 
-@shared_task
-def update_podcast(url):
+
+@shared_task(bind=True, base=RetryTask)
+def update_podcast(self,url):
     try:
         data = requests.get(url).text
         parser = Parser(data)
         parser.update_exist_podcast()
+
+        if self.request.retries > self.retry_kwargs['max_retries']:
+            logger.error(f'{self.request.retries},{self.retry_kwargs["max_retries"]}')
+        elif self.request.retries == self.retry_kwargs['max_retries']:
+            logger.error("[Task isn't successfully]")
+
     except Exception as e:
         logger.error(e)
