952e0413d97b4c8dd05ec40129881ad986100565
hosseink9 <hosseinkalantari.29@gmail.com>
2023-10-05 12:51:25 +0330

    Create login consume for consuming log-in notif

--- src/consumer.py
+++ src/consumer.py
@@ -1,9 +1,11 @@
 
+def login_consume():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+    chanel = connection.channel()
 
+    chanel.queue_declare(queue='login')
+    chanel.basic_consume(queue='login', on_message_callback=login_callback)
 
-def login_callback(chanel, method, properties, body):
-    data = json.loads(body)
-    user = User.objects.get(username=data['username'])
-    notification_info = NotificationInfo.objects.create(message=data['message'])
-    Notification.objects.create(user = user, message = notification_info)
+    chanel.start_consuming()
+
 
