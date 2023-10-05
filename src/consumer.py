6f605c3bb1e907a917672cf8c4726038ace70742
hosseink9 <hosseinkalantari.29@gmail.com>
2023-10-05 12:54:25 +0330

    Create register callback for give register info

--- src/consumer.py
+++ src/consumer.py
@@ -28,3 +28,9 @@ def login_consume():
     chanel.start_consuming()
 
 
+def register_callback(chanel, method, properties, body):
+    data = json.loads(body)
+    user = User.objects.get(username=data['username'])
+    notification_info = NotificationInfo.objects.create(message=data['message'])
+    Notification.objects.create(user = user, message = notification_info)
+
