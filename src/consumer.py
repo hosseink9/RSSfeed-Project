e97f09372bec6a5608c5ca6022b1ead106ececdb
hosseink9 <hosseinkalantari.29@gmail.com>
2023-10-05 12:50:11 +0330

    Create login callback for give log in info

--- src/consumer.py
+++ src/consumer.py
@@ -1,11 +1,9 @@
-import pika
-import json
-import time
-import os
 
-import django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-django.setup()
 
-from users.models import User, Notification, NotificationInfo
-from feedback.models import Playlist
+
+def login_callback(chanel, method, properties, body):
+    data = json.loads(body)
+    user = User.objects.get(username=data['username'])
+    notification_info = NotificationInfo.objects.create(message=data['message'])
+    Notification.objects.create(user = user, message = notification_info)
+
