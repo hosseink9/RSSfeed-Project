d1cc41a68e78b7d5e8df9ec529d06a9c600267e8
hosseink9 <hosseinkalantari.29@gmail.com>
2023-10-05 13:03:16 +0330

    Create login func to give info and publish it

--- src/main/publisher.py
+++ src/main/publisher.py
@@ -9,3 +9,13 @@ class Publish:
         result = self.chanel.queue_declare(queue = '', exclusive = True)
         self.qname = result.method.queue
 
+    def login(self, username, request_META):
+        notification={
+            'username' : username,
+            'message' : f"{username} is login; {request_META}"
+        }
+        self.response = None
+        # self.corr_id = str(uuid.uuid4())
+        self.chanel.basic_publish(exchange = '', routing_key = 'login',
+                                    properties = pika.BasicProperties(delivery_mode=2), body =json.dumps(notification)) # properties = pika.BasicProperties(reply_to = self.qname, correlation_id=self.corr_id)
+
