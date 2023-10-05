fe578d4d8f3191b1049a052705a950c68e86796e
hosseink9 <hosseinkalantari.29@gmail.com>
2023-10-05 13:02:21 +0330

    Create Publish and add init for start connection

--- src/main/publisher.py
+++ src/main/publisher.py
@@ -2,3 +2,10 @@ import pika
 import uuid
 import json
 
+class Publish:
+    def __init__(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+        self.chanel = self.connection.channel()
+        result = self.chanel.queue_declare(queue = '', exclusive = True)
+        self.qname = result.method.queue
+
