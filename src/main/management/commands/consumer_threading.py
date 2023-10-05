56269b059de285cb6cddc11899dc1f716b732fb4
hosseink9 <hosseinkalantari.29@gmail.com>
2023-10-05 13:06:20 +0330

    Create Command class for start 3 consumers together

--- src/main/management/commands/consumer_threading.py
+++ src/main/management/commands/consumer_threading.py
@@ -7,3 +7,14 @@ from consumer import login_consume, register_consume, update_podcast_consume
 import threading
 
 
+class Command(BaseCommand):
+
+    def handle(self, *args: Any, **options: Any):
+
+        login_thread =threading.Thread(target=login_consume)
+        register_thread =threading.Thread(target=register_consume)
+        update_podcast_thread =threading.Thread(target=update_podcast_consume)
+
+        login_thread.start()
+        register_thread.start()
+        update_podcast_thread.start()
