752717d6f727f2f5d11022708a163c4346d0469d
hosseink9 <hosseinkalantari.29@gmail.com>
2023-10-05 13:10:20 +0330

    Add it for give info

--- src/users/views.py
+++ src/users/views.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
 from rest_framework.views import Response
+from main.publisher import Publish
 
 import jwt,datetime
 
