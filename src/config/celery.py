bd1c5cfed5d9302eb881744b183cdc9a35692bcf
hosseink9 <hosseinkalantari.29@gmail.com>
2023-10-08 16:45:05 +0330

    Add 2 different lines for rate limiting

--- src/config/celery.py
+++ src/config/celery.py
@@ -1,10 +1,13 @@
 import os
-
 from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 app = Celery('config')
+
+# task_annotations = {'*': {'rate_limit': '1/m'}}
+
+app.conf.task_default_rate_limit = '1/m'
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
