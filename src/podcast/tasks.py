5bac643e5217eac672ac0e2a9e10ceaa78abf32f
hosseink9 <hosseinkalantari.29@gmail.com>
2023-10-08 16:46:37 +0330

    Edit and Fixed save podcast task

--- src/podcast/tasks.py
+++ src/podcast/tasks.py
@@ -17,17 +17,14 @@ class RetryTask(Task):
 
 @shared_task(bind=True, base=RetryTask)
 def save_podcast(self,url):
-    try:
-        logger.info(msg='Podcast going to parsing')
-        data = requests.get(url).text
-        podcast_url = PodcastUrl.objects.get(url=url)
-        Parser(podcast_url=podcast_url, rss_file=data, save=True)
 
-        if self.request.retries > self.retry_kwargs['max_retries']:
-            logger.error(f'{self.request.retries},{self.retry_kwargs["max_retries"]}')
-        elif self.request.retries == self.retry_kwargs['max_retries']:
-            logger.error("[Task isn't successfully]")
+    logger.info(msg='Podcast going to parsing')
+    data = requests.get(url).text
+    podcast_url = PodcastUrl.objects.get(url=url)
+    Parser(podcast_url=podcast_url, rss_file=data, save=True)
 
-    except ConnectionError as e:
-        logger.error(e)
-    return 'OK'
+    if self.request.retries > self.retry_kwargs['max_retries']:
+        logger.error(f'{self.request.retries},{self.retry_kwargs["max_retries"]}')
+    elif self.request.retries == self.retry_kwargs['max_retries']:
+        logger.error("Task isn't successfully")
+    return 'Podcast going to parsing'
