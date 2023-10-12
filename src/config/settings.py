4a75ea07a2f962b771cb0110abfd352dfd692a03
hosseink9 <hosseinkalantari.29@gmail.com>
2023-10-12 09:06:34 +0330

    Add our middleware for logging API and celery

--- src/config/settings.py
+++ src/config/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'main.middleware.LoggingMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
