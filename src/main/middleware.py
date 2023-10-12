b102b21e3a3c4378813247fd0193dc632334fb8c
hosseink9 <hosseinkalantari.29@gmail.com>
2023-10-12 09:35:06 +0330

    Create this class for use in middleware and logging with our logformater

--- src/main/middleware.py
+++ src/main/middleware.py
@@ -12,3 +12,17 @@ def log_to_elasticsearch(log_data, log_level):
         logger.error(json.dumps(log_data))
 
 
+class LoggingMiddleware:
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        log_data = log_format(request, response)
+        log_to_elasticsearch(log_data, log_level='info')
+        return response
+
+    def process_exception(self, request, exception):
+        log_data = log_format(request, None, exception)
+        log_to_elasticsearch(log_data, log_level='error')
