7c6a71215134fdc2229ea48e3bfd85524ba1bb5f
hosseink9 <hosseinkalantari.29@gmail.com>
2023-10-12 09:29:25 +0330

    Create ElasticHandler for add log and handler error

--- src/main/logger_handler.py
+++ src/main/logger_handler.py
@@ -9,3 +9,17 @@ from config import settings
 
 tz = pytz.timezone(settings.TIME_ZONE)
 
+class ElasticHandler(logging.Handler):
+
+    def __init__(self):
+        super().__init__()
+        self.es = Elasticsearch(f'http://{settings.ELASTICSEARCH_HOST}:{settings.ELASTICSEARCH_PORT}')
+        self.sender = LogSender(self.es)
+
+    def emit(self, record):
+        try:
+            self.sender.writeLog(record, formatter=self.format)
+        except Exception:
+            self.handleError(record)
+
+
