aa09a172a2462d616554c91d4d6a356d3bc048dc
hosseink9 <hosseinkalantari.29@gmail.com>
2023-10-12 09:29:58 +0330

    Create LogSender for send send and save log

--- src/main/logger_handler.py
+++ src/main/logger_handler.py
@@ -23,3 +23,14 @@ class ElasticHandler(logging.Handler):
             self.handleError(record)
 
 
+class LogSender:
+    def __init__(self, es):
+        self.es = es
+
+    def writeLog(self, msg: logging.LogRecord, formatter):
+        index_name = f'log_date{time.strftime("%Y_%m_%d")}'
+        timestamp = datetime.now().strftime('%d/%b/%Y:%H:%M:%S +03:30')
+        log_data = json.loads(formatter(msg))
+        log_data['timestamp'] = timestamp
+
+        self.es.index(index=index_name, document=log_data)
