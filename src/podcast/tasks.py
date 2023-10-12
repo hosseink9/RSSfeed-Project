461a7f2c269828dfc601476b2771b5a00467456f
hosseink9 <hosseinkalantari.29@gmail.com>
2023-10-12 09:58:47 +0330

    Add some options for RetryTask

--- src/podcast/tasks.py
+++ src/podcast/tasks.py
@@ -10,6 +10,11 @@ class RetryTask(Task):
     autoretry_for = (Exception,)
     retry_kwargs = {'max_retries': 5}
     retry_backoff = 2
+    retry_jitter = False
+    task_acks_late = True
+    worker_concurrency = 4
+    prefetch_multiplier = 1
+
 
 
 
