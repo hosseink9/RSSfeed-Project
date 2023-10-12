bce94a6d5ea14be1ede7c5f8304e60007b70e9c0
hosseink9 <hosseinkalantari.29@gmail.com>
2023-10-12 10:28:01 +0330

    Fixed save podcast task

--- src/podcast/tasks.py
+++ src/podcast/tasks.py
@@ -34,13 +34,8 @@ class RetryTask(Task):
 @shared_task(bind=True, base=RetryTask)
 def save_podcast(self,url):
 
-    logger.info(msg='Podcast going to parsing')
     data = requests.get(url).text
     podcast_url = PodcastUrl.objects.get(url=url)
     Parser(podcast_url=podcast_url, rss_file=data, save=True)
 
-    if self.request.retries > self.retry_kwargs['max_retries']:
-        logger.error(f'{self.request.retries},{self.retry_kwargs["max_retries"]}')
-    elif self.request.retries == self.retry_kwargs['max_retries']:
-        logger.error("Task isn't successfully")
     return 'Podcast going to parsing'
