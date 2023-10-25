08587789017729ba07205ab2bfd87a5da853b73f
hosseink9 <hosseinkalantari.29@gmail.com>
2023-10-25 19:19:45 +0330

    Fixed log format

--- src/main/utils.py
+++ src/main/utils.py
@@ -8,12 +8,18 @@ def log_format(request, response, exception=None):
         'user_phone': request.user.phone if request.user.is_authenticated else ' ',
     }
     remote_host = request.META.get("REMOTE_ADDR",'-')
-    request_line = f"{request.method} {request.get_full_path()} HTTP/1.1"
+    request_line = request.method
+
     status_code = response.status_code if not exception else 500
-    response_size = response.get('Content-Length', ' ')
-    referer = request.META.get('HTTP_REFERER', '-')
+    response_size = response.get('Content-Length', ' ') if response else ' '
+    referrer = request.META.get('HTTP_REFERRER', '-')
     elapsed_time = response.elapsed.total_seconds() if hasattr(response, 'elapsed') else None
-    event = f"{request.resolver_match.app_names}.{request.resolver_match.url_name}"#{request.resolver_match.app_names}.{request.resolver_match.url_name}
+    user_agent=request.headers.get("user-agent")
+    event = f"{request.get_full_path()} HTTP/1.1"
+
+    # event = f"{request.resolver_match.app_names[0]}.{request.resolver_match.url_name}" if request.resolver_match.app_names else ' '
+    # event = f"api.{request.resolver_match.app_name}.{request.resolver_match.url_name}"
+    # event = request.resolver_match.url_name
 
     message = str(exception) if exception else 'Request is successfully'
 
@@ -23,8 +29,9 @@ def log_format(request, response, exception=None):
         'request_line': request_line,
         'status_code': status_code,
         'response_size': response_size,
-        'referer': referer,
+        'referrer': referrer,
         'elapsed_time': elapsed_time,
         'message': message,
-        'event': event
+        'user_agent': user_agent,
+        'event': event,
     }
