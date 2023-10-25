cb38a6289ce385baca29119348fcf80f1a928d55
hosseink9 <hosseinkalantari.29@gmail.com>
2023-10-25 19:20:46 +0330

    Add app name and fixed recommendation url

--- src/podcast/urls.py
+++ src/podcast/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import PodcastListView, AddPodcastView, LikeView, CommentView, PlaylistView, UpdatePodcastView, AddPodcastUrlView
+from .views import PodcastListView, AddPodcastView, LikeView, CommentView, PlaylistView, UpdatePodcastView, AddPodcastUrlView,RecommendationView
+
+app_name = 'podcast'
 
 urlpatterns = [
     path("podcasts/", PodcastListView.as_view(), name="podcasts"),
@@ -8,5 +10,6 @@ urlpatterns = [
     path("like/", LikeView.as_view(),name='like'),
     path("comment/", CommentView.as_view(),name='comment'),
     path("playlist/", PlaylistView.as_view(),name='playlist'),
+    path("recommendation/", RecommendationView.as_view(),name='recommendation'),
     path("update/", UpdatePodcastView.as_view(),name='update'),
 ]
