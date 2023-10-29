60fbed5cf09043ad9e7817c17acd5a38239296ed
hosseink9 <hosseinkalantari.29@gmail.com>
2023-10-29 12:54:25 +0330

    FIxed some fields in episode serializer

--- src/episode/serializers.py
+++ src/episode/serializers.py
@@ -4,4 +4,4 @@ from .models import Episode
 class EpisodeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Episode
-        fields = ['title','guid','itunes_episode_type','itunes_explicit','description','enclosure','link','pub_date','itunes_keywords','itunes_player','podcast','episode_author']
+        fields = ['title','guid','itunes_episode_type','itunes_explicit','description','enclosure','link','pubDate','itunes_keywords','itunes_player','episode_podcast','episode_author']
