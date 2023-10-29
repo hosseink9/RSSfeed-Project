bf7fb0245b27c16791923dd6e88ee84c1ba993d2
hosseink9 <hosseinkalantari.29@gmail.com>
2023-10-29 12:55:06 +0330

    Add a new url for see a episode

--- src/episode/urls.py
+++ src/episode/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import EpisodeListView
+from .views import EpisodeListView, EpisodeView
 
 app_name = 'episode'
 
 urlpatterns = [
-    path('episodes/', EpisodeListView.as_view(), name='episode')
+    path('episodes/', EpisodeListView.as_view(), name='episode'),
+    path('episode_view/<id>', EpisodeView.as_view(), name='episode_view')
 ]
